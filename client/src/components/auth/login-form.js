de188f084d79211924ec4b1185a7a58b646996f4
paulnchinnam <paulnchinnam@icloud.com>
2022-06-24 18:38:54 -0500

    formatted the login screen to look a little better

--- client/src/components/auth/login-form.js
+++ client/src/components/auth/login-form.js
@@ -92,29 +92,44 @@ export default function LoginForm() {
 
                 <p className="max-w-md mx-auto mt-4 text-center text-gray-500">Planning your day, made easy.</p>
 
-                <form action="" className="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl" onSubmit={(e) => loginUser(e)}>
-                    <p className="text-lg font-medium">Log into your accout</p>
+                <div className="shadow-2xl rounded-lg">
+                    <form action="" className="p-10 mt-6 mb-0 space-y-4 rounded-t-lg" onSubmit={(e) => loginUser(e)}>
+                        <p className="text-2xl font-light text-center pb-10">Log in to your account</p>
 
-                    <div>
-                        <label htmlFor="email" className="text-sm font-medium">Email</label>
+                        <div className="pb-2">
+                            <label htmlFor="email" className="text-sm font-medium">Email address</label>
 
-                        <div className="relative mt-1">
-                            <input type="email" id="email" className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm" placeholder="Enter email" value={email} onChange={(e) => {setEmail(e.target.value)}} />
+                            <div className="relative mt-1">
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3.5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-gray-400" fill="none" viewBox="0 0 25 25" stroke-width="1">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                    </svg>
+                                </span>
+
+                                <input type="email" id="email" className="w-full pl-11 p-4 pr-12 text-sm border-gray-200 rounded-lg border-2 border-gray-200" placeholder="Enter your email" value={email} onChange={(e) => {setEmail(e.target.value)}} />
+                            </div>
                         </div>
-                    </div>
 
-                    <div>
-                        <label htmlFor="password" className="text-sm font-medium">Password</label>
+                        <div className="pb-2">
+                            <label htmlFor="password" className="text-sm font-medium">Password</label>
 
-                        <div className="relative mt-1">
-                            <input type="password" id="password" className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm" placeholder="Enter password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+                            <div className="relative mt-1">
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3.5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-gray-400" fill="none" viewBox="0 0 25 25" stroke-width="1">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+                                    </svg>
+                                </span>
+
+                                <input type="password" id="password" className="w-full pl-11 p-4 pr-12 text-sm border-gray-200 rounded-lg border-2 border-gray-200" placeholder="Enter your password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+                            </div>
                         </div>
+
+                        <button type="submit" input={+true} value="submit" className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg"><span className="text-xl font-light">Log In</span></button>
+                    </form>
+                    <div className="p-6 bg-gray-100 rounded-b-lg border-t-2 border-gray-200">
+                        <p className="text-md text-light text-center text-gray-500">New to Task App? <a className="underline text-indigo-600" href="">Sign Up</a></p>
                     </div>
-
-                    <button type="submit" input={+true} value="submit" className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg">Sign in</button>
-
-                    <p className="text-sm text-center text-gray-500">No Account? <a className="underline" href="">Sign Up</a></p>
-                </form>
+                </div>
             </div>
         </div>
     )
