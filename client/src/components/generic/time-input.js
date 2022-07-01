619840df7766bb04e7d543af75f6d7f500e4214f
paulnchinnam <paulnchinnam@icloud.com>
2022-06-30 22:55:00 -0500

    changed time input to original

--- client/src/components/generic/time-input.js
+++ client/src/components/generic/time-input.js
@@ -16,97 +16,22 @@ function TimeInput(props) {
 
   return (
     <form onSubmit={(e) => formatTime(e)}>
-    <div class="mt-2 p-5 w-40">
-      <div class="flex">
-        <select
-          name="hours"
-          class="bg-transparent text-xl appearance-none outline-none"
-        >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">10</option>
-          <option value="12">12</option>
+        <input type="number" placeholder="12" id="start-hours" className=" bg-transparent text-xl appearance-none outline-none" />
+        <input type="number" placeholder="00" id="start-minutes" className="border-none" />
+        <select id="startampm">
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
         </select>
-        <span class="text-xl mr-3">:</span>
-        <select
-          name="minutes"
-          class="bg-transparent text-xl appearance-none outline-none mr-4"
-        >
-          <option value="0" id="start-hours">00</option>
-          <option value="30" id="start-minutes">30</option>
+        <br />
+        <input type="number" placeholder="12" id="end-hours"/>
+        <input type="number" placeholder="00" id="end-minutes"/>
+        <select id="endampm">
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
         </select>
-        <select
-          class="bg-transparent text-xl appearance-none outline-none"
-          id="endampm"
-        >
-          <option value="AM">AM</option>
-          <option value="PM">PM</option>
-        </select>
-      </div>
-    </div>
-    <div class="mt-2 p-5 w-40">
-      <div class="flex">
-        <select
-          name="hours"
-          class="bg-transparent text-xl appearance-none outline-none"
-        >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">10</option>
-          <option value="12">12</option>
-        </select>
-        <span class="text-xl mr-3">:</span>
-        <select
-          name="minutes"
-          class="bg-transparent text-xl appearance-none outline-none mr-4"
-        >
-          <option value="0" id="end-hours">00</option>
-          <option value="30" id="end-minutes">30</option>
-        </select>
-        <select
-          class="bg-transparent text-xl appearance-none outline-none"
-          id="endampm"
-        >
-          <option value="AM">AM</option>
-          <option value="PM">PM</option>
-        </select>
-      </div>
-    </div>
-    <button type="submit" value="submit" className="bg-indigo-600 rounded-lg px-5 py-3 font-medium text-white">Submit</button>
+        <br />
+        <button type="submit" value="submit" className="bg-indigo-600 rounded-lg px-5 py-3 font-medium text-white">Submit</button>
     </form>
-    // <form onSubmit={(e) => formatTime(e)}>
-    //     <input type="number" placeholder="12" id="start-hours" className="border-none" />
-    //     <input type="number" placeholder="00" id="start-minutes" className="border-none" />
-    //     <select id="startampm">
-    //         <option value="AM">AM</option>
-    //         <option value="PM">PM</option>
-    //     </select>
-    //     <br />
-    //     <input type="number" placeholder="12" id="end-hours"/>
-    //     <input type="number" placeholder="00" id="end-minutes"/>
-    //     <select id="endampm">
-    //         <option value="AM">AM</option>
-    //         <option value="PM">PM</option>
-    //     </select>
-    //     <br />
-    //     <button type="submit" value="submit" className="bg-indigo-600 rounded-lg px-5 py-3 font-medium text-white">Submit</button>
-    // </form>
   );
 }
 
