bf194754440efec79759cd8378e9d64786f9f20e
Ryan McCord <mccordryan00@gmail.com>
2022-11-29 02:20:56 -0600

    put the stat component on overview page

--- client/src/components/Overview/Overview.js
+++ client/src/components/Overview/Overview.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-
+import TaskStatGraph from "../statistics/TaskStatGraph";
 import "tw-elements";
 
 export default function Overview() {
   return <>Overview page
-
+    <TaskStatGraph></TaskStatGraph>
   </>;
 }
