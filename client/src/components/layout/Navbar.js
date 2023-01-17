a8ca60ab9d59b0c0c27b4559245f2e93e5af8702
paulnchinnam <paulnchinnam@icloud.com>
2023-01-17 14:14:18 -0600

    added more landing page content

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,28 +6,29 @@ export default function Navbar() {
 
   return (
     <>
-      <nav class="max-w-screen-2xl flex mx-auto justify-between pt-6 fixed-top">
-        <a href="/">
-          <h1 className="text-2xl font-semibold">Jigsaw</h1>
-        </a>
-        <div class="space-x-5 text-xl">
-          <button
-            type="button"
-            class="text-gray-500 hover:text-gray-900 font-normal font-sans"
-            onClick={() => history("/login")}
-          >
-            Sign in
-          </button>
-          <button
-            type="button"
-            class="text-white rounded-md bg-gray-900 hover:bg-gray-700 font-normal px-6 py-2 font-sans"
-            onClick={() => history("/register")}
-          >
-            Sign up
-          </button>
-        </div>
-      </nav>
-
+        <nav class="fixed-top bg-white">
+          <div className="max-w-screen-2xl flex mx-auto justify-between py-6">
+          <a href="/">
+            <h1 className="text-2xl font-semibold">Jigsaw</h1>
+          </a>
+          <div class="space-x-5 text-xl">
+            <button
+              type="button"
+              class="text-gray-500 hover:text-gray-900 font-normal font-sans"
+              onClick={() => history("/login")}
+            >
+              Sign in
+            </button>
+            <button
+              type="button"
+              class="text-white rounded-md bg-gray-900 hover:bg-gray-700 font-normal px-6 py-2 font-sans"
+              onClick={() => history("/register")}
+            >
+              Sign up
+            </button>
+          </div>
+          </div>
+        </nav>
       {/* <nav class="max-w-4xl flex items-center mx-auto p-3 xs:px-4 xs:pt-3 sm:p-4 justify-between">
         <a href="/" class="inline-flex items-center space-x-3">
           <img alt="logo" src="https://flowbite.com/docs/images/logo.svg" />
--- client/src/pages/HeroSection2.js
+++ client/src/pages/HeroSection2.js
@@ -22,9 +22,7 @@ export default function HeroSection2() {
                 adapt to your progress
               </p>
               <div className="space-x-5 max-w-sm flex text-center">
-                <span
-                  className="bg-white border w-full text-lg font-sans py-4 border-gray-600 hover:text-gray-900 text-gray-600 rounded-md hover:bg-sidebarColor hover:border-gray-900"
-                >
+                <span className="bg-white border w-full text-lg font-sans py-4 border-gray-600 hover:text-gray-900 text-gray-600 rounded-md hover:bg-sidebarColor hover:border-gray-900">
                   Learn how
                 </span>
                 <span
@@ -43,6 +41,84 @@ export default function HeroSection2() {
           </div>
         </div>
       </section>
+
+      <section className="grid grid-cols-2 py-20 bg-neutral-50">
+        <div className="place-self-center max-w-xl">
+          <img src="https://i.imgur.com/mnbxD36.png" alt="" />
+        </div>
+        <div className="place-self-center max-w-xl">
+          <h1 className="text-5xl text-gray-900 pb-4">
+            <span className="highlight">How Jigsaw works</span>
+          </h1>
+          <p className="text-2xl text-gray-500 font-sans font-light">
+            Using our custom sorting algorithms, Jigsaw builds an adaptive
+            schedule from a list of tasks you create. The schedule reorganizes
+            itself based on the progress of your tasks throughout the day so
+            that you can focus on the work at hand and avoid feeling overwhelmed
+            by your busy life.
+          </p>
+        </div>
+      </section>
+
+      <div className="py-10">
+      <section className="grid grid-cols-2 py-20">
+        <div className="place-self-center max-w-xl">
+          <h1 className="text-5xl text-gray-900 pb-4">
+            <span className="highlight">Tasks</span>
+          </h1>
+          <p className="text-2xl text-gray-500 font-sans font-light">
+            Using Jigsaw starts with the task library. You can create tasks to add to your library and easily add and remove from your schedule. Jigsaw's powerful scheduling is derived from a few key metrics you provide when creating task including priority, time-of-day, and length of time to complete the task.
+          </p>
+        </div>
+        <div className="place-self-center max-w-xl">
+          <img
+            src="https://i.imgur.com/2n1Fq9t.png"
+            alt=""
+            className="border border-gray-900 rounded-lg custom-image-box-shadow"
+          />
+        </div>
+      </section>
+
+      <section className="grid grid-cols-2 py-20">
+        <div className="place-self-center max-w-xl">
+          <img
+            src="https://i.imgur.com/uUCeVH3.png"
+            alt=""
+            className="border border-gray-900 rounded-lg custom-image-box-shadow"
+          />
+        </div>
+        <div className="place-self-center max-w-xl">
+          <h1 className="text-5xl text-gray-900 pb-4">
+            <span className="highlight">Schedule</span>
+          </h1>
+          <p className="text-2xl text-gray-500 font-sans font-light">
+            Once tasks are added from your library to the schedule, Jigsaw uses the metrics you provided to organize your day for you. You no longer need to decide which tasks need to be completed at which times. Jigsaw takes care of all the planning so you can focus on the work at hand.
+          </p>
+        </div>
+      </section>
+
+      <section className="grid grid-cols-2 py-20">
+        <div className="place-self-center max-w-xl">
+          <h1 className="text-5xl text-gray-900 pb-4">
+            <span className="highlight">Analysis</span>
+          </h1>
+          <p className="text-2xl text-gray-500 font-sans font-light">
+            Jigsaw analysis a variety of metrics regarding your task completion progress and presents them to you in a simple and understandable way. You can get an idea of your efficiency for the current day and ways to improve the next.
+          </p>
+        </div>
+        <div className="place-self-center max-w-xl">
+          <img
+            src="https://i.imgur.com/40FihzM.png"
+            alt=""
+            className="border border-gray-900 rounded-lg custom-image-box-shadow"
+          />
+        </div>
+      </section>
+      </div>
+
+      <footer className="py-20 bg-neutral-50">
+
+      </footer>
     </>
   );
 }
