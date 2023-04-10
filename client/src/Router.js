d812b1b13c14e1033b37ffcc524c67fb45cfd941
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-04-09 20:44:54 -0500

    removed duplicate files and updated the hero section and navbar components

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,40 +1,38 @@
-import React from "react"
-import  {BrowserRouter, Routes, Route } from "react-router-dom"
-import LoginForm from "./components/auth/login-form"
-import RegisterForm from "./components/auth/register-form"
-import TaskPage from "./pages/task-page"
-import SchedulePage from "./pages/schedule-page"
-import HomePage from "./pages/home-page"
-import Dashboard from "./components/layout/Dashboard"
-import AccountSettings from "./components/AccountSettings/AccountSettings"
-import Help from "./components/Help/Help"
-import Overview from "./components/Overview/Overview"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/auth/login-form";
+import RegisterForm from "./components/auth/register-form";
+import TaskPage from "./pages/task-page";
+import SchedulePage from "./pages/schedule-page";
+import Dashboard from "./components/layout/Dashboard";
+import AccountSettings from "./components/AccountSettings/AccountSettings";
+import Help from "./components/Help/Help";
+import Overview from "./components/Overview/Overview";
+import HomePage from "./pages/HomePage";
 
-export default function Router(...restParams){
+export default function Router(...restParams) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Landing page route */}
+        <Route exact path="/" element={<HomePage />} />
 
-    return (
-        <BrowserRouter>
-            <Routes>
+        {/* <Route exact path="/schedule" element={<SchedulePage/>} /> */}
+        {/* <Route path="/tasks" element={<TaskPage/>} /> */}
 
-                {/* Landing page route */}
-                <Route exact path ="/" element={<HomePage />} />
-                
-                {/* <Route exact path="/schedule" element={<SchedulePage/>} /> */}
-                {/* <Route path="/tasks" element={<TaskPage/>} /> */}
+        {/* login & register page routes */}
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
 
-                {/* login & register page routes */}
-                <Route path="/login" element={<LoginForm />} />
-                <Route path="/register" element={<RegisterForm/>} />
-
-                {/* Dashboard routes */}
-                <Route path="/dashboard/" element={<Dashboard />}>
-                    <Route path="schedule" element={<SchedulePage />} />
-                    <Route path="tasks" element={<TaskPage />} />
-                    <Route path="accountSettings" element={<AccountSettings />} />
-                    <Route path="help" element={<Help />} />
-                    <Route path="overview" element={<Overview />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )  
+        {/* Dashboard routes */}
+        <Route path="/dashboard/" element={<Dashboard />}>
+          <Route path="schedule" element={<SchedulePage />} />
+          <Route path="tasks" element={<TaskPage />} />
+          <Route path="accountSettings" element={<AccountSettings />} />
+          <Route path="help" element={<Help />} />
+          <Route path="overview" element={<Overview />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,16 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 // import Navbar from "../components/layout/Navbar";
 import Navbar from "../components/layout/Navbar";
-import HeroSection from "./hero-section";
-import BetaHeroSection from "./BetaHeroSection";
+import HeroSection from "../components/layout/HeroSection";
 import HeroSection2 from "./HeroSection2";
 
 export default function HomePage() {
-  const history = useNavigate();
-
   return (
     <>
-      <HeroSection2></HeroSection2>
+      <Navbar></Navbar>
+      <HeroSection></HeroSection>
     </>
   );
 }
