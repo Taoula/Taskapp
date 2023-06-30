2b44d8180e7d4e577e2cc897b8fb1dfdb5b18a2e
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-06-30 16:45:23 -0500

    functional task options to edit and delete

--- client/src/components/tasks/Task.js
+++ client/src/components/tasks/Task.js
@@ -103,8 +103,12 @@ export default function Task({ task, getTasks }) {
                 <div className="py-1">
                   <Menu.Item>
                     <button
-                      onClick={() => setShowUpdateTask(true)}
-                      className="flex px-4 py-2 text-sm font-normal"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setOpen2(true);
+                      }}
+                      className={` hover:bg-gray-100 hover:text-slate-900 text-gray-700
+                  flex justify-between w-full px-4 py-2 text-sm font-normal`}
                     >
                       <span>Edit</span>
                     </button>
@@ -112,7 +116,8 @@ export default function Task({ task, getTasks }) {
                   <Menu.Item>
                     <button
                       onClick={deleteTask}
-                      className="flex px-4 py-2 text-sm font-normal"
+                      className={` hover:bg-red-100 hover:text-red-600 text-gray-700
+                  flex justify-between w-full px-4 py-2 text-sm font-normal`}
                     >
                       <span>Delete</span>
                     </button>
@@ -123,176 +128,15 @@ export default function Task({ task, getTasks }) {
           </Menu>
         </div>
       </div>
+
+      {/* update task form */}
+      <UpdateTaskSlideover
+        open2={open2}
+        setOpen2={setOpen2}
+        getTasks={getTasks}
+        currentDay={currentDay}
+        _id={_id}
+      ></UpdateTaskSlideover>
     </>
-    // <>
-    //   <TaskContainer
-    //     color={colors[priority - 1]}
-    //     style={{ border: `solid ${borderColors[priority - 1]} 1px` }}
-    //     onClick={toggle}
-    //     className="rounded-md px-5 py-4 flex items-center justify-between"
-    //   >
-    //     <div className="flex items-center space-x-2">
-    //       <span>
-    //         {isActive ? (
-    //           <CheckSquare size={20} onClick={toggleActive} />
-    //         ) : (
-    //           <Square size={20} onClick={toggleActive} />
-    //         )}
-    //       </span>
-    //       <span>
-    //         <h1 className="capitalize font-normal text-md">
-    //           {name}: <span className="font-light">{duration} minutes</span>
-    //         </h1>
-    //       </span>
-    //     </div>
-    //     {/* <DotsThree size={32} /> */}
-    //     <div className="flex space-x-1">
-    //       <span className="hover:text-gray-900">
-    //         <DotsThreeOutline
-    //           size={20}
-    //           weight="fill"
-    //           // onClick={() => setShowUpdateTask(true)}
-    //           onClick={() => setOpen2(true)}
-    //           className="text-gray-700"
-    //         />
-    //       </span>
-    //       {time != null && <span>{convertTime(time, "utc")}</span>}
-    //     </div>
-
-    //     <div className="flex">
-    //       <p>Delete:</p>
-    //       <Trash size={25} weight="fill" onClick={deleteTask} />
-    //     </div>
-    //     {/*
-
-    //       <div>
-    //         {isActive ? (
-    //           <CheckSquare size={20} onClick={toggleActive} />
-    //         ) : (
-    //           <Square size={20} onClick={toggleActive} />
-    //         )}
-    //       </div> */}
-
-    //     {/* <div>
-    //         <p
-    //           className="hover:underline"
-    //           onClick={() => setShowUpdateTask(true)}
-    //         >
-    //           Edit Task
-    //         </p>
-    //         <Pencil size={25} weight="fill" />
-    //       </div> */}
-    //   </TaskContainer>
-
-    //   <DeleteTaskDialogue
-    //     deleteTaskDialogue={deleteTaskDialogue}
-    //     setDeleteTaskDialogue={setDeleteTaskDialogueOpen}
-    //     getTasks={getTasks}
-    //     task={task}
-    //   ></DeleteTaskDialogue>
-
-    //   <UpdateTaskSlideover
-    //     open2={open2}
-    //     setOpen2={setOpen2}
-    //     getTasks={getTasks}
-    //     currentDay={currentDay}
-    //     _id={_id}
-    //   ></UpdateTaskSlideover>
-
-    // {/* <UpdateTaskForm
-    //   getTasks={getTasks}
-    //   _id={_id}
-    //   visible={showUpdateTask}
-    //   onClose={handleUpdateOnClose}
-    // /> */}
-    // </>
-
-    // <>
-    //   <TaskContainer
-    //     color={colors[priority - 1]}
-    //     style={{ border: `solid ${borderColors[priority - 1]} 1px` }}
-    //     onClick={toggle}
-    //     className="rounded-md px-5 py-4 flex items-center justify-between"
-    //   >
-    //     <div className="flex items-center space-x-2">
-    //       <span>
-    //         {isActive ? (
-    //           <CheckSquare size={20} onClick={toggleActive} />
-    //         ) : (
-    //           <Square size={20} onClick={toggleActive} />
-    //         )}
-    //       </span>
-    //       <span>
-    //         <h1 className="capitalize font-normal text-md">
-    //           {name}: <span className="font-light">{duration} minutes</span>
-    //         </h1>
-    //       </span>
-    //     </div>
-    //     {/* <DotsThree size={32} /> */}
-    //     <div className="flex space-x-1">
-    //       <span className="hover:text-indigo-600">
-    //         <PencilSimple
-    //           size={20}
-    //           weight="light"
-    //           // onClick={() => setShowUpdateTask(true)}
-    //           onClick={() => setOpen2(true)}
-    //         />
-    //       </span>
-    //       <span className="hover:text-red-500">
-    //         <Trash
-    //           size={20}
-    //           weight="light"
-    //           // onClick={deleteTask}
-    //           onClick={() => setDeleteTaskDialogueOpen(true)}
-    //         />
-    //       </span>
-    //       {time != null && <span>{convertTime(time, "utc")}</span>}
-    //     </div>
-
-    //     {/* <div className="flex">
-    //         <p>Delete:</p>
-    //         <Trash size={25} weight="fill" onClick={deleteTask} />
-    //       </div>
-
-    //       <div>
-    //         {isActive ? (
-    //           <CheckSquare size={20} onClick={toggleActive} />
-    //         ) : (
-    //           <Square size={20} onClick={toggleActive} />
-    //         )}
-    //       </div> */}
-
-    //     {/* <div>
-    //         <p
-    //           className="hover:underline"
-    //           onClick={() => setShowUpdateTask(true)}
-    //         >
-    //           Edit Task
-    //         </p>
-    //         <Pencil size={25} weight="fill" />
-    //       </div> */}
-    //   </TaskContainer>
-
-    //   <DeleteTaskDialogue
-    //     deleteTaskDialogue={deleteTaskDialogue}
-    //     setDeleteTaskDialogue={setDeleteTaskDialogueOpen}
-    //     getTasks={getTasks}
-    //     task={task}
-    //   ></DeleteTaskDialogue>
-
-    // <UpdateTaskSlideover
-    //   open2={open2}
-    //   setOpen2={setOpen2}
-    //   getTasks={getTasks}
-    //   _id={_id}
-    // ></UpdateTaskSlideover>
-
-    //   {/* <UpdateTaskForm
-    //     getTasks={getTasks}
-    //     _id={_id}
-    //     visible={showUpdateTask}
-    //     onClose={handleUpdateOnClose}
-    //   /> */}
-    // </>
   );
 }
