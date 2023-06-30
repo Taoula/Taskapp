834119b9f87836fba7f896ffde8f9bbc2b6601f4
Ryan McCord <mccordryan00@gmail.com>
2023-06-29 20:07:59 -0500

    minor changes

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,8 +5,7 @@ const userSchema = new mongoose.Schema({
     lName: {type: String, required: true},
     userRole: {type: String, required: true},
     email: {type: String, required: true},
-    passwordHash: {type: String, required: true},
-    settings: {type: Array, required: true}
+    passwordHash: {type: String, required: true}
 })
 
 const User = mongoose.model("user", userSchema);
